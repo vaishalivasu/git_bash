90aaba9fbc6c1fbe4b3ccb778b6a7d0322ae04aa
vaishalivasu <vaishali.v1920@gmail.com>
2025-03-01 12:48:30 +0530

    commit1

---
+++ src/test/java/tekPyramid/sample/DemoTest.java
@@ -0,0 +1,10 @@
+package tekPyramid.sample;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		System.out.println("Demo");
+
+	}
+
+}
---
+++ src/test/java/tekPyramid/sample/SampleTest.java
@@ -0,0 +1,9 @@
+package tekPyramid.sample;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
